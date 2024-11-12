58d9ee307aaab90eaf6cef4977cf4b007b23bd82
Aldo <aldopereira.gyn@gmail.com>
2024-11-11 21:46:17 -0300

    Inserindo inserts para os Games: Game Seed

--- src/main/java/com/dev/dslist/entities/Game.java
+++ src/main/java/com/dev/dslist/entities/Game.java
@@ -12,25 +12,30 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-
+    private Double score;
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public  Game() {
 
     }
 
-    public Game(Long id, String title, String genre, String platform, String imgUrl, String shortDescription, String longDescription, Integer year) {
+    public Game(Long id, String title, Double score, Integer year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
-        this.year = year;
         this.title = title;
+        this.score = score;
+        this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -60,12 +65,28 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return platform;
+    public Double getScore() {
+        return score;
     }
 
-    public void setPlataform(String plataform) {
-        this.platform = plataform;
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public String getImgUrl() {
